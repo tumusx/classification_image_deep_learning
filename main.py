7b3b3d72be2aaf375e5ddc514d9e284e2f0bc7b4
Murillo Alves <tumusx1@gmail.com>
2023-07-06 20:34:59 -0300

    chore: add comments

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
-from PIL import Image, ImageOps  # Install pillow instead of PIL
-import numpy as np
-import matplotlib.pyplot as plt
+from keras.models import load_model  # Importa a função load_model do Keras para carregar o modelo treinado
+from PIL import Image, ImageOps  # Importa as classes Image e ImageOps da biblioteca PIL (Pillow)
+import numpy as np  # Importa a biblioteca numpy para trabalhar com arrays numéricos
+import matplotlib.pyplot as plt  # Importa a biblioteca matplotlib para criar gráficos
 
-# Disable scientific notation for clarity
+# Define as opções de impressão para desativar a notação científica
 np.set_printoptions(suppress=True)
 
-# Load the model
+# Carrega o modelo treinado a partir do arquivo "model/keras_model.h5"
 model = load_model("model/keras_model.h5", compile=False)
 
-# Load the labels
+# Carrega os rótulos das classes a partir do arquivo "model/labels.txt"
 class_names = open("model/labels.txt", "r").readlines()
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1
+# Cria o array de formato apropriado para alimentar o modelo do Keras
+# O formato é (1, 224, 224, 3), onde 1 é o número de imagens, 224 é a largura e altura da imagem, e 3 é o número de canais de cor (RGB)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# Replace this with the path to your image
+# Abre a imagem a ser classificada a partir do arquivo "images/bad_image.jpg" e converte para o modo RGB
 image = Image.open("images/bad_image.jpg").convert("RGB")
 
-# resizing the image to be at least 224x224 and then cropping from the center
+# Redimensiona a imagem para ter no mínimo 224x224 pixels e faz um recorte a partir do centro
 size = (224, 224)
 image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-# turn the image into a numpy array
+# Converte a imagem em um array do NumPy
 image_array = np.asarray(image)
 
-# Normalize the image
+# Normaliza o array da imagem, convertendo-o para float32, dividindo por 127.5 e subtraindo 1
 normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-# Load the image into the array
+# Carrega o array da imagem normalizada para o array 'data'
 data[0] = normalized_image_array
 
-# Predicts the model
+# Faz a previsão usando o modelo
 prediction = model.predict(data)
+
+# Obtém o índice da classe com a maior probabilidade de previsão
 index = np.argmax(prediction)
+
+# Obtém o nome da classe correspondente ao índice encontrado
 class_name = class_names[index]
+
+# Obtém a pontuação de confiança da previsão
 confidence_score = prediction[0][index]
 
+# Cria um gráfico de linha para visualizar a acurácia
 plt.plot([0, 1], [confidence_score, confidence_score], color='blue')
 
 # Configurações do gráfico
@@ -50,8 +56,9 @@ plt.ylabel('Valor')
 plt.xlim(0, 1)
 plt.ylim(min(confidence_score, 0), max(confidence_score, 1))
 
-# Exibir o gráfico
+# Salva o gráfico em um arquivo
 plt.savefig('grafico.png')
-# Print prediction and confidence score
+
+# Imprime a classe prevista e a pontuação de confiança
 print("Class:", class_name[2:], end="")
 print("Confidence Score:", confidence_score)
